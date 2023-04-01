8a4a1aef9ca7a554cbbd0ada67cf72d92fdfa835
huchang <799207539@qq.com>
2023-04-02 02:14:13 +0800

    add python bytecode extended arg example

---
+++ code/codeobject/write_bytecode.py
@@ -0,0 +1,23 @@
+
+import dis
+
+
+def num_to_byte(n):
+    return n.to_bytes(1, "little")
+
+
+def nums_to_bytes(data):
+
+    ans = b"".join([num_to_byte(n) for n in data])
+    return ans
+
+
+if __name__ == '__main__':
+    # extended_arg extended_num opcode oparg for python_version > 3.5
+    bytecode = nums_to_bytes([144, 1, 144, 2, 100, 65])
+    print(bytecode)
+    dis.dis(bytecode)
+
+    bytecode = nums_to_bytes([100, 65])
+    print(bytecode)
+    dis.dis(bytecode)
