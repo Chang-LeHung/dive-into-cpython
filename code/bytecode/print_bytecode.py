029da567cfccef902f77b668acd15e8f5b54fd8e
changlehung <changlehung@qq.com>
2023-05-08 13:51:29 +0800

    update

--- code/bytecode/print_bytecode.py
+++ code/bytecode/print_bytecode.py
@@ -4,6 +4,15 @@ import dis
 
 
 if __name__ == '__main__':
-    for (key, val) in dis.opmap.items():
+    i = 0
+    items = list(dis.opmap.items())
+    while i < len(items):
+        key, val = items[i]
         s = "\\_".join(key.split("_"))
-        print(f"{s} & {val} \\\\ \\hline")
+        print(f"{s} & {val} &", end="")
+        i += 1
+
+        if i < len(items) - 1:
+            s = "\\_".join(key.split("_"))
+            print(f"{s} & {val} \\\\ \\hline")
+            i += 1
