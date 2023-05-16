1c045c6fa473d4096cf0b043351599bea080fdd2
changlehung <changlehung@qq.com>
2023-05-17 01:53:12 +0800

    update deserilize pyc

--- code/marshal_demos/add.py
+++ code/marshal_demos/add.py
@@ -11,4 +11,20 @@ if __name__ == '__main__':
     with open("add.bin", "wb") as fp:
         marshal.dump(add.__code__, fp)
 
+    with open("int.bin", "wb") as fp:
+        marshal.dump(1, fp)
+    with open("float.bin", "wb") as fp:
+        marshal.dump(1.5, fp)
+    with open("tuple.bin", "wb") as fp:
+        marshal.dump((1, 2, 3), fp)
+    with open("set.bin", "wb") as fp:
+        marshal.dump({1, 2, 3}, fp)
+    with open("list.bin", "wb") as fp:
+        marshal.dump([1, 2, 3], fp)
+    with open("dict.bin", "wb") as fp:
+        marshal.dump({1: 2, 3: 4}, fp)
+    with open("code.bin", "wb") as fp:
+        marshal.dump(add.__code__, fp)
 
+    with open("string.bin", "wb") as fp:
+        marshal.dump("Hello World", fp)
--- code/marshal_demos/parser.py
+++ code/marshal_demos/parser.py
@@ -1,123 +1,12 @@
-# 深入理解 python 虚拟机：破解核心魔法——反序列化 pyc 文件
 
-在前面的文章当中我们详细的对于 pyc 文件的结构进行了分析，pyc 文件主要有下面的四个部分组成：魔术、 Bite Filed 、修改日期和 Code Object 组成。在前面的文章当中我们已经对前面三个部分进行了字节角度的分析，直接从 pyc 文件当中读取对应的数据并且打印出来了。而在本篇文章当中我们将主要对 Code Object 进行分析，并且详细它是如何被反序列化的，通过本篇文章我们将能够把握整个 pyc 文件结构。
-
-![36-pyc](../images/38-pyc.png)
-
-## marshal 模块的魔力
-
-### 序列化和反序列化 python 对象
-
-marshal 是 python 自带的一个模块，他可以将一些 python 内置对象进行序列化和反序列化操作，甚至我们可以在一个文件当中序列话一个函数的 Code Object 对象，然后在另外一个文件反序列化这个 Code Object 对象并且执行它。
-
-我们可以使用下面的代码将 python 当中的一些对象序列化操作，直接将 python 对边变成一个字节流，保存到磁盘当中：
-
-```python
-import marshal
+import struct
+from enum import Enum
+from pprint import pprint
+import sys
 
 
-if __name__ == '__main__':
-    with open("pyobjects.bin", "wb") as fp:
-        marshal.dump(1, fp)
-        marshal.dump(1.5, fp)
-        marshal.dump("Hello World", fp)
-        marshal.dump((1, 2, 3), fp)
-        marshal.dump([1, 2, 3], fp)
-        marshal.dump({1, 2, 3}, fp)
-        marshal.dump(1+1j, fp)
-        marshal.dump({1: 2, 3: 4}, fp)
-```
-
-在上面的代码当中需要注意的是需要使用二进制方式 rb 打开文件，上面的程序执行完成之后会生成一个 pyobjects.bin 的二进制文件，我们可以使用 python 代码再将上面的 python 对象，比如整数、浮点数、字符串和列表元组等等反序列化出来。
-
-```python
-import marshal
-
-
-if __name__ == '__main__':
-    with open("pyobjects.bin", "rb") as fp:
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-        print(marshal.load(fp))
-```
-
-上面的代码输出结果如下所示：
-
-```python
-1
-1.5
-Hello World
-(1, 2, 3)
-[1, 2, 3]
-{1, 2, 3}
-(1+1j)
-{1: 2, 3: 4}
-```
-
-从上面代码的输出结果我们可以看到我们可以将所有的被写入到二进制文件当中的数据全部解析了出来。
-
-### 序列化和反序列化 CodeObject
-
-除了上面使用 marshal 对 python 的基本对象进行序列化和反序列化，我们可以使用 marshal 模块对 CodeObject 进行同样的操作，如果是这样的话，那么就可以将一个文件的代码序列化，然后另外一个程序反序列化再进行调用：
-
-```python
-import marshal
-
-
-def add(a, b):
-    print("Hello World")
-    return a+b
-
-
-with open("add.bin", "wb") as fp:
-    marshal.dump(add.__code__, fp)
-```
-
-在上面的代码当中，我们打开了文件 `add.bin` 然后将 add 函数的 CodeObject 对象写入到文件当中去，而 CodeObject 当中保存了函数 add 的所有执行所需要的信息，因此我们可以在另外一个文件当中打开这个文件，然后将 CodeObject 对象反序列化出来在执行这个代码，我们看下面的代码：
-
-```python
-import marshal
-
-
-def name():
-    pass
-
-
-with open("add.bin", "rb+") as fp:
-    code = marshal.load(fp)
-name.__code__ = code
-print(name(1, 2))
-```
-
-上面的代码执行结果如下所示：
-
-```bash
-Hello World
-3
-```
-
-可以看到反序列化之后的函数 add 复制到了 name 上，然后我们调用了函数 name 真的实现了打印和相加的效果，从这一点来看确实实现了我们在前面所提到的效果。
-
-## Python 对象反序列化
-
-在本节当中将主要分析 python 对象序列化之后的二进制文件格式，我们到底应该如何解析这个文件，解析文件的规则是什么。在 cpython 当中对于每个数据类型的解析都是不一样的，marshal 支持 python 当中所有的基本数据类型，额外还支持 CodeObject ，在上面的验证代码当中我们已经使用 marshal 去做了一些序列化和反序列化操作。
-
-在对 python 对象进行序列化的时候，每一个 python 对象主要是由两个部分组成的：
-
-![83-pyc](../images/83-pyc.png)
-
-其中 type 占一个字节，用于表示接下里啊的 python 的对象类型，比如如字典、元组、集合之类的，而后面的 PyObject 就是实际的 python 数据类型了，需要注意的是对于 None、False、True 这种在虚拟机当中只有一个备份的对象，PyObject 是没有的，也就只有 type 这一个字段。
-
-type 的种类具体如下所示，它只占用一个字节：
-
-```python
 class TYPE(Enum):
-    TYPTYPE_NULLE_NULL                 = ord('0')
+    TYPE_NULL                 = ord('0')
     TYPE_NONE                 = ord('N')
     TYPE_FALSE                = ord('F')
     TYPE_TRUE                 = ord('T')
@@ -147,11 +36,8 @@ class TYPE(Enum):
     TYPE_SMALL_TUPLE          = ord(')')
     TYPE_SHORT_ASCII          = ord('z')
     TYPE_SHORT_ASCII_INTERNED = ord('Z')
-```
 
-我们接下来对上面的类型进行一一解释，首先我们需要了解下面几个方法，我们在后面的解析过程当中会使用到下面的内容：
 
-```python
 class ByteStreamReader(object):
 
     @staticmethod
@@ -173,41 +59,8 @@ class ByteStreamReader(object):
     @staticmethod
     def read_long(buf):
         return struct.unpack("<q", buf)[0]
-```
 
-上面的的几个函数主要是将字节变成 byte、int 或者浮点数。接下来我们会实现一个类 PyObjectLoader，用于对 marshal 序列化之后的文件进行解析。类的构造函数如下所示：
 
-```python
-class PyObjectLoader(object):
-
-    def __init__(self, filename):
-        self.fp = open(filename, "rb")
-        self.flag = 0
-        self.refs = []
-```
-
-现在来对一个对象进行解析，根据我们前面谈到的内容首先我们需要读入一个字节的内容用于判断是那种数据类型：
-
-```python
-def do_parse(self):
-    c = self.fp.read(1)
-    assert len(c) != 0, "can not read more data from file descriptor"
-    t = ByteStreamReader.read_byte(c) & (~TYPE.FLAG_REF.value)
-    self.flag = ByteStreamReader.read_byte(c) & TYPE.FLAG_REF.value
-```
-
-在上面的代码当中使用函数 do_parse 对一个 python 对象进行解析操作，使用到了 TYPE.FLAG_REF，这个字段的作用表示这个 python 对象是不是一个可引用的，除了 None 、True、False、StopIteration、Ellipsis 是不可引用对象，集合、字典、不可变集合、字符串、字节、CodeObject 等是可引用对象，可引用对象的 type 的最高位是 1（也就是 type 的第 8 个比特位是 1），非可引用对象就是 0 。
-
-- TYPE_NULL，这个在 cpython 虚拟机当中就会直接返回 NULL 。
-- TYPE_NONE，返回 python 对象 None 。
-- TYPE_FALSE，返回 python 对象 False 。
-- TYPE_TRUE，返回 python 对象 True 。
-- TYPE_STOPITER，返回 StopIteration 对象。
-- TYPE_ELLIPSIS，返回 对象 Ellipsis 。
-- TYPE_INT，如果是这个数据类型表示接下来的 4 个字节的数据是一个整数。
-- 
-
-```python
 class PyObjectLoader(object):
 
     def __init__(self, filename):
@@ -373,5 +226,24 @@ class PyObjectLoader(object):
 
     def __del_(self):
         self.fp.close()
-```
+
+
+if __name__ == '__main__':
+    assert sys.version_info.major == 3 and sys.version_info.minor == 10, "only python3.10 works"
+    loader = PyObjectLoader("int.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("float.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("set.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("dict.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("tuple.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("list.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("string.bin")
+    print(loader.do_parse())
+    loader = PyObjectLoader("code.bin")
+    pprint(loader.do_parse())
 
