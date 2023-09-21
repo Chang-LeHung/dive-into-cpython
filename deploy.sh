4e4690bb09d4906ee2e2e86d2e1fdaac391a3350
changlehung <changlehung@qq.com>
2023-09-21 18:59:11 +0800

    update deplo.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Chang-LeHung/dive-into-cpython.git master:gh-pages
+git push -f git@github.com:Chang-LeHung/dive-into-cpython.git main:gh-pages
 
 cd -
