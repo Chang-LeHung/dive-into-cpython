f549cbb37ccddfc4d127e96a1580b7011ae31aee
changlehung <changlehung@gmail.com>
2025-01-06 19:25:33 +0800

    feat: add last update and search dialog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -52,6 +52,50 @@ export default {
   base: '/dive-into-cpython/',
 
   themeConfig: {
+    // 启用最后更新时间
+    lastUpdated: true,
+    lastUpdatedText: '最后更新时间',  // 可以配置显示文本
+
+    // 文档页脚
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    
+    // 页脚配置
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2024-present Chang-LeHung'
+    },
+    
+    // 大纲配置
+    outline: {
+      level: [2, 3],     // 仅显示 h2 和 h3 标题
+      label: '目录'      // 大纲标题
+    },
+    
+    // 搜索配置
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档'
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭'
+            }
+          }
+        }
+      }
+    },
+
     // 使用硬编码的方式配置 sidebar
     sidebar: [
       {
